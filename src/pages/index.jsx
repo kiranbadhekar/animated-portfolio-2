275c16257931f36ff616a305d0fbc5c94e07535e
kiranbadhekar <kiranbadhekar8@gmail.com>
2026-01-19 10:40:27 +0530

    Fix ESLint unused imports for Netlify build

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,6 @@
-import React from "react";
 import Home from "../components/Home";
 import About from "../components/About";
 import Projects from "../components/Projects";
-import Contact from "../components/Contact";
 import "./index.css";
 
 
